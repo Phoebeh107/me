003941fa24d8537cc956c2404d7f1e89c39e2874
Phoebeh107 <51425897+Phoebeh107@users.noreply.github.com>
2019-06-20 22:58:00 +1000

    week 2 upload

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -6,8 +6,12 @@ def is_odd(a_number):
     """Return True if a_number is odd, and False if a_number is even.
 
     Look into modulo division using the '%' operator as one way of doing this.
-    """
-    return None
+    """    
+    if a_number%2 != 0: 
+        return True
+    else:
+        return False
+
 
 
 def fix_it(moves=True, should_move=True):
@@ -25,7 +29,14 @@ def fix_it(moves=True, should_move=True):
     Most people write this function with 4 return statements. 
     As an extra challenge, see if you can get that down to three.
     """
-    return None
+
+    if (moves==True and should_move==True) or (moves==False and should_move==False):
+        return 'No Problem'
+    elif (moves==True and should_move==False):
+        return 'Duct Tape'
+    else:
+        return 'WD-40'
+
 
 
 def loops_1a():
@@ -35,7 +46,10 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    stars=[]
+    for count in range(10):
+        stars.append('*')
+    return stars 
 
 
 def loops_1c(number_of_items=5, symbol="#"):
@@ -45,7 +59,11 @@ def loops_1c(number_of_items=5, symbol="#"):
     string with exacly one symbol in it.
     E.g.: ['#', '#', '#', '#', '#']
     """
-    return None
+    
+    hash_list=[]
+    for count in range(number_of_items):
+        hash_list.append(symbol)
+    return hash_list
 
 
 def loops_2():
@@ -66,7 +84,17 @@ def loops_2():
             ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
           ]
     """
-    return None
+     
+    stars_list=[]
+    stars_box=[]
+    for x in range(10):
+        for y in range(10):
+            stars_list.append('*')
+        #return stars_list
+        y=0
+        stars_box.append(stars_list) 
+        stars_list=[]
+    return stars_box
 
 
 def loops_3():
@@ -90,7 +118,16 @@ def loops_3():
     TIP: notice that this needs to to return strings of numbers,
          so call str(number) to cast.
     """
-    return None
+    numbers_list=[]
+    numbers_box=[]
+    for i in range(10):
+        for j in range(10):
+            numbers_list.append(str(i))
+        j=0
+        numbers_box.append(numbers_list) 
+        numbers_list=[]
+    return numbers_box
+    
 
 
 def loops_4():
@@ -110,7 +147,16 @@ def loops_4():
       ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     ]
     """
-    return None
+    numbers_list=[]
+    numbers_box=[]
+    for i in range(10):
+        for j in range(10):
+            numbers_list.append(str(j))
+        j=0
+        numbers_box.append(numbers_list) 
+        numbers_list=[]
+    return numbers_box
+    
 
 
 def loops_5():
@@ -137,7 +183,16 @@ def loops_5():
         "There are {} green bottles".format(8)
     you'll come to see the pros and cons of each over time.
     """
-    return None
+
+    coord_list=[]
+    coord_box=[]
+    for i in range(10):
+        for j in range(5):
+            coord_list.append("(i" + str(i) + "," + " " + "j" + str(j) + ")")
+        j=0
+        coord_box.append(coord_list) 
+        coord_list=[]
+    return coord_box
 
 
 def loops_6():
@@ -160,7 +215,17 @@ def loops_6():
     You can use a variable.
     TIP: look out for the starting condition.
     """
-    return None
+    numbers_list=[]
+    numbers_triangle=[]
+    variable=1
+    for i in range(10):
+        for j in range(variable):
+            numbers_list.append(str(j))
+        j=0
+        variable+=1
+        numbers_triangle.append(numbers_list)
+        numbers_list=[]
+    return numbers_triangle
 
 
 def loops_7():
@@ -184,7 +249,27 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    return None
+    stars_list=[]
+    stars_box=[]
+    for x in range(5):
+        for y in range(9):
+            if y<4-x or y>x+4:
+                stars_list.append(' ')
+            else:
+                stars_list.append('*')    
+                '''for z in range(2*x):
+                    stars_list.append('*')
+                    y+=1'''
+            '''elif x==1 and y==range(3,5):
+                stars_list.append('*')
+            elif x==2 and y==range(2,6):
+                stars_list.append('*')
+            elif x==3 and y==range(1,7):
+                stars_list.append('*')'''
+        y=0
+        stars_box.append(stars_list) 
+        stars_list=[]
+    return stars_box
 
 
 def lp(some_kind_of_list, exercise_name):
--- week2/readme.md
+++ week2/readme.md
@@ -1,1 +1,15 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+Learnt:
+    Uppercase string  - stringName.upper()
+    Return with space - 
+
+    for loop - for count in range('a number'):
+                    list_name.append('what needs to be returned 'a number' of times')
+                return list_name
+    imbed loop in loop - dont use return in the middle: as soon as it hits a return the function stops
+
+
+    
+
+Unclear:
+
